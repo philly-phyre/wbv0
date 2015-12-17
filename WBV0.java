0a21b2021516c1a21975c3ab6e0a3f9b473edde3
Phil Carpenter <Phil Carpenter>
2015-12-17 02:23:47 -0500

    Update

---
+++ WBV0.java
@@ -0,0 +1,19 @@
+package org.tempestdesign.webviewzero;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.web.WebView;
+import javafx.scene.web.WebEngine;
+import javafx.scene.layout.HBox;
+import javafx.stage.Stage;
+public class WBV0 extends Application {
+	private String title = "WeightLess Browser /u0020/u0020 |  | /u0020/u0020 Welcome Home";
+	@Override
+	public void start(Stage win) {
+		win.setTitle(title);
+		
+	}
+	public static void main(String args[]) {
+		launch(args);
+	}
+	
+}
